f77e777ef3286287ea819eca60a5188efd54c027
Arensis <alfonso.madrid.dev@gmail.com>
2020-05-24 19:49:36 +0200

    Added secured url for jquery script

--- js/nav.js
+++ js/nav.js
@@ -4,10 +4,10 @@ $(function () {
 
 function setNavigation() {
     var path = window.location.pathname;
-    console.log(path)
+
     $(".nav a").each(function () {
         var href = $(this).attr('href');
-        console.log(path.includes(href))
+
         if (path.includes(href)) {
             $(this).closest('a').addClass('active');
         }
